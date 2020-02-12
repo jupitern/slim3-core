866a9afbd5dc4362829bfbe7cfdbb340ea966e0e
jupitern <nmpchaves@gmail.com>
2020-02-12 18:49:53 +0000

    .

--- src/ServiceProviders/Monolog.php
+++ src/ServiceProviders/Monolog.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace Jupitern\Slim3\ServiceProviders;
-use Psr\Log\LoggerInterface;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Formatter\LineFormatter;
 use Monolog\Processor\WebProcessor;
 use Monolog\Handler\SISHandler;
 use Monolog\Handler\SyslogUdpHandler;
+use Monolog\Handler\DataDogHandler;
 
 class Monolog implements ProviderInterface
 {
@@ -19,27 +19,36 @@ class Monolog implements ProviderInterface
         $formatter->includeStacktraces(false);
 
         foreach ($settings as $logger) {
-            if ($logger['type'] == 'sis' && (bool)$logger['enabled']) {
-                $handler = new SISHandler($logger['host'], $logger['apiKey'], $logger['level']);
-                $monolog->pushHandler($handler);
-                $monolog->pushProcessor(new WebProcessor());
-            
-            } elseif ($logger['type'] == 'file' && (bool)$logger['enabled']) {
+
+            if ($logger['type'] == 'file' && (bool)$logger['enabled']) {
+                $formatter = new LineFormatter(null, null, true);
+                $formatter->includeStacktraces(true);
+
                 $handler = new StreamHandler($logger['path'], $logger['level']);
                 $handler->setFormatter($formatter);
                 $monolog->pushHandler($handler);
             
+            } elseif ($logger['type'] == 'sis' && (bool)$logger['enabled']) {
+                $handler = new SISHandler($logger['host'], $logger['apiKey'], $logger['level']);
+                $monolog->pushHandler($handler);
+                $monolog->pushProcessor(new WebProcessor());
+            
             } elseif ($logger['type'] == 'papertrail' && (bool)$logger['enabled']) {
                 $output = "%channel%.%level_name%: %message%";
                 $formatter = new LineFormatter($output);
-
+                
                 $handler = new SyslogUdpHandler($logger['host'], $logger['port']);
                 $handler->setFormatter($formatter);
                 $monolog->pushHandler($handler);
+
+            } elseif ($logger['type'] == 'datadog' && (bool)$logger['enabled']) {
+                $handler = new DataDogHandler($logger['host'], $logger['apiKey'], $logger['appKey'], $logger['level']);
+                $monolog->pushHandler($handler);
+            
             }
         }
 
-	app()->getContainer()['logger'] = $monolog;
+	    app()->getContainer()['logger'] = $monolog;
     }
 
 }
