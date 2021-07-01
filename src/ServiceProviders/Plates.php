1e84b53416b9fbd59c1bf5e25f43754796296046
Rodrigo Alves <23285553+imrodrigoalves@users.noreply.github.com>
2021-07-01 17:12:07 +0100

    Beautify Code

--- src/ServiceProviders/Plates.php
+++ src/ServiceProviders/Plates.php
@@ -1,25 +1,24 @@
 <?php
 
 namespace Jupitern\Slim3\ServiceProviders;
+
 use League\Plates\Engine;
 
 class Plates implements ProviderInterface
 {
-
-	public static function register($serviceName, array $settings = [])
-	{
+    public static function register($serviceName, array $settings = [])
+    {
         $engine = new Engine();
         foreach ($settings['templates'] as $name => $path) {
             $engine->addFolder($name, $path, true);
         }
-        
-        if(array_key_exists('extensions', $settings)){
-            foreach($settings['extensions'] as $extension){
+
+        if (array_key_exists('extensions', $settings)) {
+            foreach ($settings['extensions'] as $extension) {
                 $engine->loadExtension(new $extension);
             }
         }
 
-		app()->getContainer()[$serviceName] = $engine;
-	}
-
+        app()->getContainer()[$serviceName] = $engine;
+    }
 }
